529d2d24c99732cffdd6b9126154bce25a159b39
yoke <1352444134@qq.com>
2019-12-15 15:17:49 +0800

    update

--- spring-boot-explore/spring-boot-dive/chapter03-spring-boot-application/src/main/java/com/yoke/Chapter03Application.java
+++ spring-boot-explore/spring-boot-dive/chapter03-spring-boot-application/src/main/java/com/yoke/Chapter03Application.java
@@ -3,9 +3,22 @@ package com.yoke;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+/**
+ * {@link SpringApplication} 需要主配置类，主配置类需要使用{@link SpringBootApplication} 注解修饰
+ * {@link SpringBootApplication} 是一个模式注解，集合了
+ *  {@link org.springframework.context.annotation.Configuration}
+ *  {@link org.springframework.boot.autoconfigure.EnableAutoConfiguration}
+ *  {@link org.springframework.context.annotation.ComponentScan}
+ */
+//@SpringBootApplication
 public class Chapter03Application {
     public static void main(String[] args) {
-        new SpringApplication(Chapter03Application.class).run(args);
+        new SpringApplication(ApplicationConfig.class).run(args);
+        System.out.println("----");
+    }
+
+    @SpringBootApplication
+    public static class ApplicationConfig{
+
     }
 }

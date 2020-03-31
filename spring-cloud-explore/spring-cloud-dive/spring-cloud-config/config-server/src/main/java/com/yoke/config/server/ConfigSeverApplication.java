0910ad509edcffefad658e1f54db8e9a23ef3e75
wyw <1352444134@qq.com>
2020-03-31 19:52:54 +0800

    update

--- spring-cloud-explore/spring-cloud-dive/spring-cloud-config/config-server/src/main/java/com/yoke/config/server/ConfigSeverApplication.java
+++ spring-cloud-explore/spring-cloud-dive/spring-cloud-config/config-server/src/main/java/com/yoke/config/server/ConfigSeverApplication.java
@@ -5,6 +5,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import sun.security.krb5.Config;
 
+/**
+ * spring-cloud-config-server可以通过下列形式访问(GET)配置文件
+ * http://localhost:port/{application}/{profile}[/{label}]
+ * http://localhost:port/{application}-{profile}.yml
+ * http://localhost:port/{label}/{application}-{profile}.yml
+ * http://localhost:port/{application}-{profile}.properties
+ * http://localhost:port/{label}/{application}-{profile}.properties
+ * application为配置文件名
+ * profile为配置文件环境
+ * label为分支
+ */
 @SpringBootApplication
 @EnableConfigServer
 public class ConfigSeverApplication {
